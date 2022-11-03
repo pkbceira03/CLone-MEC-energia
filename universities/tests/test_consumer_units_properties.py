133c9ba6fe8bd6e6f961a358f68942641678d3a3
Bruno-Felix <balvesfelix@gmail.com>
2022-11-03 15:12:41 -0300

    feat: Consertando lógica teste

--- universities/tests/test_consumer_units_properties.py
+++ universities/tests/test_consumer_units_properties.py
@@ -3,30 +3,48 @@ import pytest
 from datetime import date
 from rest_framework import status
 from rest_framework.test import APIClient
-from django.contrib.auth.models import User
 
+from users.models import UniversityUser
 from universities.models import University, ConsumerUnit
 from contracts.models import Contract, EnergyBill
 
 ENDPOINT = '/api/consumer-units/'
-USERNAME = 'admin'
 EMAIL = 'admin@admin.com'
 PASSWORD = 'admin@admin.com'
 
 @pytest.mark.django_db
 class TestConsumerUnitsProperties:
-    def setup(self):
+    def test_reads_consumer_unit_properties_no_energy_bill_filled(self):
+        response = self.client.get(f'{ENDPOINT}{self.consumer_unit_1.id}/')
+        consumer_unit = json.loads(response.content)
+
+        assert self.contract_1.start_date == consumer_unit['date']
+        assert False == consumer_unit['is_current_energy_bill_filled']
+        assert 10 == consumer_unit['pending_energy_bills_number']
+        assert status.HTTP_200_OK == response.status_code
+
+    def test_reads_consumer_unit_properties_current_energy_bill_filled(self):
+        response = self.client.get(f'{ENDPOINT}{self.consumer_unit_2.id}/')
+        geted_consumer_unit = json.loads(response.content)
+
+        assert geted_consumer_unit['date'] == self.contract_2.start_date
+        assert geted_consumer_unit['is_current_energy_bill_filled'] == True
+        assert geted_consumer_unit['pending_energy_bills_number'] == 5
+        assert response.status_code == status.HTTP_200_OK
+
+
+    def setup_method(self):
         self.client = APIClient()
         self.date_now = date.today()
-        self.user = User.objects.create_user(
-            username=USERNAME, email=EMAIL, password=PASSWORD)
-        self.client.login(username=USERNAME, password=PASSWORD)
         university_dict = {
             'name': 'Universidade de Brasília',
             'cnpj': '00038174000143'
         }
         self.university = University(**university_dict)
         self.university.save()
+        self.user = UniversityUser.objects.create_user(
+            email=EMAIL, password=PASSWORD, university=self.university)
+        self.client.login(email=EMAIL, password=PASSWORD)
         self.consumer_unit_1_dict = {
             'name': 'Darcy Ribeiro',
             'code': '000000000',
@@ -46,7 +64,7 @@ class TestConsumerUnitsProperties:
         self.consumer_unit_2 = ConsumerUnit(**self.consumer_unit_2_dict)
         self.consumer_unit_2.save()
         self.contract_1_dict = {
-            'start_date': f'2022-01-01',
+            'start_date': '2022-01-01',
             'end_date': '2023-01-01',
             'tariff_flag': 'V',
             'sub_group': 'A1',
@@ -82,22 +100,3 @@ class TestConsumerUnitsProperties:
         }
         self.energy_bill = EnergyBill(**self.energy_bill_dict)
         self.energy_bill.save()
-
-    def test_reads_consumer_unit_properties_no_energy_bill_filled(self):
-        response = self.client.get(f'{ENDPOINT}{self.consumer_unit_1.id}/')
-        geted_consumer_unit = json.loads(response.content)
-
-        assert geted_consumer_unit['date'] == self.contract_1.start_date
-        assert geted_consumer_unit['is_current_energy_bill_filled'] == False
-        assert geted_consumer_unit['pending_energy_bills_number'] == 10
-        assert response.status_code == status.HTTP_200_OK
-
-    """ def test_reads_consumer_unit_properties_current_energy_bill_filled(self):
-        response = self.client.get(f'{ENDPOINT}{self.consumer_unit_2.id}/')
-        geted_consumer_unit = json.loads(response.content)
-
-        assert geted_consumer_unit['date'] == self.contract_2.start_date
-        assert geted_consumer_unit['is_current_energy_bill_filled'] == True
-        assert geted_consumer_unit['pending_energy_bills_number'] == 5
-        assert response.status_code == status.HTTP_200_OK """
-    
