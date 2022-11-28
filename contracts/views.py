66f55d1a3cd2c64bb33e4137d0b4615c38fc6510
Bruno Alves Félix <balvesfelix@gmail.com>
2022-11-28 01:48:46 +0000

    feat: alterando escolha subgroup e criando endpoint de listagem dos subgroups

--- contracts/views.py
+++ contracts/views.py
@@ -1,11 +1,25 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.request import Request
+from django.http import JsonResponse
 
 from . import models
 from . import serializers
 
+from utils.subgroup_util import Subgroup
+
+
 class ContractViewSet(viewsets.ModelViewSet):
     queryset = models.Contract.objects.all()
     serializer_class = serializers.ContractSerializer
+
+    @action(detail=False, methods=['get'], url_path='list-subgroups')
+    def list_subgroups(self, request: Request, pk=None):
+        subgroups = Subgroup.get_all_subgroups()
+
+        return JsonResponse(subgroups, safe=False)
+
 
 class EnergyBillViewSet(viewsets.ModelViewSet):
     queryset = models.EnergyBill.objects.all()
--- utils/subgroup_util.py
+++ utils/subgroup_util.py
@@ -1,24 +1,62 @@
 class Subgroup:
-    A1 = 'A1'
-    A2 = 'A2'
-    A3 = 'A3'
-    A3A = 'A3a'
-    A4 = 'A4'
     AS = 'AS'
+    A4 = 'A4'
+    A3A = 'A3a'
+    A3 = 'A3'
+    A2 = 'A2'
+    A1 = 'A1'
+
+    subgroups = [
+        {
+            "name": AS,
+            "min": 0,
+            "max": 2.3,
+            "equals": None,
+        },
+        {
+            "name": A4,
+            "min": 2.3,
+            "max": 25,
+            "equals": None,
+        },
+        {
+            "name": A3A,
+            "min": 30,
+            "max": 44,
+            "equals": None,
+        },
+        {
+            "name": A3,
+            "min": None,
+            "max": None,
+            "equals": 69,
+        },
+        {
+            "name": A2,
+            "min": 88,
+            "max": 138,
+            "equals": None,
+        },
+        {
+            "name": A1,
+            "min": 230,
+            "max": 999999,
+            "equals": None,
+        }
+    ]
 
 
     def get_subgroup(supply_voltage_in_kv: float):
-        if supply_voltage_in_kv < 2.3:
-            return Subgroup.AS
-        elif supply_voltage_in_kv >= 2.3 and supply_voltage_in_kv < 25:
-            return Subgroup.A4
-        elif supply_voltage_in_kv >= 30 and supply_voltage_in_kv < 44:
-            return Subgroup.A3A
-        elif supply_voltage_in_kv == 69:
-            return Subgroup.A3
-        elif supply_voltage_in_kv >= 88 and supply_voltage_in_kv < 138:
-            return Subgroup.A2
-        elif supply_voltage_in_kv >= 230:
-            return Subgroup.A1
-        else:
-            raise Exception('Subgroup not found for this supply voltage')
+        for subgroup in Subgroup.subgroups:
+            if subgroup['min'] and subgroup['max']:
+                if supply_voltage_in_kv >= subgroup['min'] and supply_voltage_in_kv < subgroup['max']:
+                    return subgroup['name']
+            
+            if subgroup['equals']:
+                if supply_voltage_in_kv == subgroup['equals']:
+                    return subgroup['name']
+        
+        raise Exception('Subgroup not found for this supply voltage')
+
+    def get_all_subgroups():
+        return Subgroup.subgroups
