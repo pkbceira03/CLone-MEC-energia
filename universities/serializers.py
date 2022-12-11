f24bdf5b8f70bef10a05c48cafbec2a03aaa05b2
Bruno Alves Félix <balvesfelix@gmail.com>
2022-12-11 20:05:40 +0000

    feat: Organizando endpoints de universidade e consumer_unit

--- universities/serializers.py
+++ universities/serializers.py
@@ -5,36 +5,6 @@ from utils.cnpj_validator_util import CnpjValidator
 from .models import ConsumerUnit, University
 
 
-class ConsumerUnitInRetrieveUniversitySerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-
-    class Meta:
-        model = ConsumerUnit
-        fields = [
-            'id', 'url', 'name', 'code', 'is_active', 'date', 'is_current_energy_bill_filled', 'pending_energy_bills_number'
-        ]
-
-
-class RetrieveUniversitySerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-
-    consumer_units = ConsumerUnitInRetrieveUniversitySerializer(
-        read_only=True,
-        many=True,
-    )
-
-    class Meta:
-        model = University
-        fields = '__all__'
-
-    def validate_cnpj(self, cnpj: str):
-        try:
-            CnpjValidator.validate(cnpj)
-        except Exception as e:
-            raise serializers.ValidationError(str(e.args))
-        return cnpj
-
-
 class UniversitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = University
@@ -51,4 +21,7 @@ class UniversitySerializer(serializers.HyperlinkedModelSerializer):
 class ConsumerUnitSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ConsumerUnit
-        fields = ['id', 'url', 'name', 'code', 'is_active', 'date', 'university']
+        fields = ['id', 'url', 'name', 'code', 'is_active', 'date', 'is_current_energy_bill_filled', 'pending_energy_bills_number', 'university']
+
+class ConsumerUnitParamsSerializer(serializers.Serializer):
+    university_id = serializers.IntegerField()
