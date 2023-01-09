bd2b342433e2240514300705038d133dedb3b048
Bruno-Felix <balvesfelix@gmail.com>
2023-01-09 01:37:58 -0300

    Padronizando busca das contas para recomendação, caso a conta atual seja preenchida

--- universities/recommendation.py
+++ universities/recommendation.py
@@ -1,4 +1,5 @@
 from datetime import date
+from dateutil.relativedelta import relativedelta
 
 from utils.energy_bill_util import EnergyBillUtils
 from contracts.models import EnergyBill
@@ -10,7 +11,9 @@ class Recommendation:
         energy_bills = []
 
         try:
-            energy_bills_dates = EnergyBillUtils.generate_dates_for_recommendation(date.today())
+            date_for_recommendation = Recommendation.set_date_for_recommendation(consumer_unit_id)
+
+            energy_bills_dates = EnergyBillUtils.generate_dates_for_recommendation(date_for_recommendation)
 
             for energy_bill_object in energy_bills_dates:
                 energy_bill = EnergyBill.get_energy_bill(
@@ -23,19 +26,6 @@ class Recommendation:
                 
                 energy_bills.append(energy_bill_object)
 
-            # A Conta de Luz do mês atual somente é considerada para a recomendação caso seja preechida
-            current_energy_bill = EnergyBill.get_energy_bill(
-                    consumer_unit_id,
-                    date.today().month, 
-                    date.today().year)
-
-            if current_energy_bill:
-                energy_bill = EnergyBillUtils.create_energy_bill_date(current_energy_bill.date.month, current_energy_bill.date.year)
-                energy_bill['energy_bill'] = EnergyBillUtils.energy_bill_dictionary(current_energy_bill)
-
-                energy_bills.insert(0, energy_bill)
-                energy_bills.pop()
-
             return energy_bills
         except Exception as e:
             raise Exception('Error get energy bills for recommendation: ' + str(e))
@@ -43,7 +33,9 @@ class Recommendation:
     @classmethod
     def get_all_energy_bills_by_consumer_unit(cls, consumer_unit_id, start_date):
         try:
-            energy_bills_recommendation_dates_list = EnergyBillUtils.generate_dates_for_recommendation(date.today())
+            date_for_recommendation = Recommendation.set_date_for_recommendation(consumer_unit_id)
+
+            energy_bills_recommendation_dates_list = EnergyBillUtils.generate_dates_for_recommendation(date_for_recommendation)
             energy_bills_lists = EnergyBillUtils.generate_dates(start_date, date.today())
             
             for years in energy_bills_lists:
@@ -66,3 +58,18 @@ class Recommendation:
             return energy_bills_lists
         except Exception as e:
             raise Exception('Error get all energy bills by consumer unit: ' + str(e))
+
+    # A Conta de Luz do mês atual somente é considerada para a recomendação caso seja preechida
+    @classmethod
+    def set_date_for_recommendation(cls, consumer_unit_id):
+        date_for_recommendation = date.today()
+        
+        current_energy_bill = EnergyBill.get_energy_bill(
+                    consumer_unit_id,
+                    date.today().month, 
+                    date.today().year)
+
+        if current_energy_bill:
+            date_for_recommendation += relativedelta(months = 1)
+
+        return date_for_recommendation
