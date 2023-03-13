67bb883e6cb9951c25b5c311538a893d3f1730d4
Bruno-Felix <bruno-felix@gmail.com>
2023-03-13 19:05:30 -0300

    feat: Aprimorando testes

--- tests/test_utils/dicts_utils.py
+++ tests/test_utils/dicts_utils.py
@@ -8,6 +8,11 @@ university_dict_1 = {
     'cnpj': '63025530000104'
 }
 
+university_dict_2 = {
+    'name': 'Universidade de Brasília',
+    'cnpj': '00038174000143'
+}
+
 # ---------------------------------------
 # Unidades Consumidoras
 
@@ -15,13 +20,13 @@ consumer_unit_dict_1 = {
     'name': 'Darcy Ribeiro',
     'code': '000000000',
     'is_active': True,
-},
+}
 
 consumer_unit_dict_2 = {
     'name': 'Faculdade do Gama',
     'code': '111111111',
     'is_active': True,
-},
+}
 
 consumer_unit_dict_3 = {
     'name': 'Faculdade de Planaltina',
@@ -38,7 +43,7 @@ contract_dict_1 = {
     'supply_voltage': 100.00,
     'peak_contracted_demand_in_kw': 100.00,
     'off_peak_contracted_demand_in_kw': 100.00,
-},
+}
 
 contract_dict_2 = {
     'start_date': date(year=2023, month = 1, day = 1),
@@ -46,7 +51,7 @@ contract_dict_2 = {
     'supply_voltage': 250.00,
     'peak_contracted_demand_in_kw': 250.00,
     'off_peak_contracted_demand_in_kw': 250.00,
-},
+}
 
 contract_dict_3 = {
     'start_date': date(year=2024, month = 1, day = 1),
@@ -54,7 +59,7 @@ contract_dict_3 = {
     'supply_voltage': 69,
     'peak_contracted_demand_in_kw': 250.00,
     'off_peak_contracted_demand_in_kw': 250.00,
-},
+}
 
 contract_dict_4 = {
     'start_date': date(year=2025, month = 1, day = 1),
@@ -62,7 +67,7 @@ contract_dict_4 = {
     'supply_voltage': 100.00,
     'peak_contracted_demand_in_kw': 100.00,
     'off_peak_contracted_demand_in_kw': 100.00,
-},
+}
 
 contract_dict_5 = {
     'start_date': date(year=2050, month = 1, day = 1),
@@ -70,7 +75,7 @@ contract_dict_5 = {
     'supply_voltage': 70,
     'peak_contracted_demand_in_kw': 250.00,
     'off_peak_contracted_demand_in_kw': 250.00,
-},
+}
 
 contract_dict_6 = {
     'start_date': date(year=2023, month = 1, day = 1),
@@ -78,7 +83,7 @@ contract_dict_6 = {
     'supply_voltage': 100.00,
     'peak_contracted_demand_in_kw': 100.00,
     'off_peak_contracted_demand_in_kw': 100.00,
-},
+}
 
 # ---------------------------------------
 # Conta de Luz
@@ -87,7 +92,7 @@ energy_bill_dict_1 = {
     'date': date.today(),
     'invoice_in_reais': 100.00,
     'is_atypical': False,
-    'peak_consumption_in_kwh': 100.00,
+    'peak_consumption_in_kwh': 100.00,p
     'off_peak_consumption_in_kwh': 100.00,
     'peak_measured_demand_in_kw': 100.00,
     'off_peak_measured_demand_in_kw': 100.00,
@@ -99,7 +104,7 @@ energy_bill_dict_1 = {
 distributor_dict_1 = {
     'name': 'Neoenergia',
     'cnpj': '01083200000118'
-},
+}
 
 distributor_dict_2 = {
     'name': 'CEB',
@@ -119,4 +124,39 @@ tariff_dict_1 = {
     'off_peak_tusd_in_reais_per_kw': 4,
     'off_peak_tusd_in_reais_per_mwh': 5,
     'off_peak_te_in_reais_per_mwh': 6
+}
+
+# ---------------------------------------
+# Users
+
+super_user_dict_1 = {
+    "first_name": "super",
+    "last_name": "admin",
+    "email": "admin@admin.com",
+    "password": "12345",
+    "type": "super_user"
+}
+
+university_user_dict_1 = {
+    "first_name": "Arnold",
+    "last_name": "Schwarzenegger",
+    "email": "arnold@user.com",
+    "password": "12345",
+    "type": "university_user"
+}
+
+university_user_dict_2 = {
+    "first_name": "Ronnie",
+    "last_name": "Coleman",
+    "email": "ronnie@user.com",
+    "password": "12345",
+    "type": "university_user"
+}
+
+university_user_dict_3 = {
+    "first_name": "Phil",
+    "last_name": "Heath",
+    "email": "phil@user.com",
+    "password": "12345",
+    "type": "university_user"
 }
